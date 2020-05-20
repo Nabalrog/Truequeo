f7d6813d7b8347c0e0227f049b55583e9dfd8a0f
Nabalrog <navarrogc_395@hotmail.com>
2020-05-19 20:26:22 -0500

    Contenedores y Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import AppNav from './components/AppNav/AppNav';
 import 'bootstrap/dist/css/bootstrap.css';
-import Header from './components/Header/Header';
+import MainContainer from "./containers/MainContainer/MainContainer";
+import MiPerfil from "./containers/MiPerfil/MiPerfil";
+import Categorias from "./containers/Categorias/Categorias";
+import Login from "./containers/Login/Login";
+import NotFound from "./containers/NotFound/NotFound"
+
 
 function App() {
-  return (<div>
-    <AppNav />
-    <Header />
-
-  </div>
-
-
+  return (
+    <BrowserRouter>
+      <AppNav />
+      <Switch>
+        <Route exact path="/MainContainer" component={MainContainer} />
+        <Route exact path="/MiPerfil" component={MiPerfil} />
+        <Route exact path="/Caregorias" component={Categorias}/>
+        <Route exact path="/Login" component={Login}/>
+        {/* Siempre es el último */}
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/containers/PerfilVisitado/PerfilVisitado.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PerfilVisitado = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PerfilVisitado
